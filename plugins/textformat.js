38c1f082c8bbe87314d7ea67084caae8cc7e3a23
Ariel <sonyt605@gmail.com>
2024-12-06 23:08:09 +0800

    add plugin textformat & timeformat

--- plugins/textformat.js
+++ plugins/textformat.js
@@ -1,1 +1,11 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("textformat", (el, binding) => {
+    const convertTextFormat = {
+      // binding.value 傳入給指令的值
+      lowercase: binding.value.toLowerCase(),
+      uppercase: binding.value.toUpperCase(),
+    };
+    // arg   => 傳入給指令的參數
+    el.innerText = convertTextFormat[binding.arg];
+  });
+});
--- plugins/timeformat.js
+++ plugins/timeformat.js
@@ -1,1 +1,14 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("timeformat", {
+    // el：綁定指令的 DOM 元素。binding：指令的參數，包含傳入的值等資訊。
+    mounted(el, binding) {
+      // binding.value 傳入給指令的值
+      const convertTimeFormat = new Date(binding.value)
+        .toLocaleString("zh-Hant", {
+          hour12: false,
+        })
+        .replaceAll("/", "-");
+      el.innerText = convertTimeFormat;
+    },
+  });
+});
