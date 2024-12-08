ace9071094fddedb1130160a9993d16eb0ab71b9
Ariel <sonyt605@gmail.com>
2024-12-08 14:13:58 +0800

    add loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <script setup>
+const { $useLoading } = useNuxtApp();
+const loadingHandler = $useLoading({
+  // options
+  color: "#02de84",
+  loader: "bars",
+  "is-full-page": false
+});
 function openLoading() {
   // 開啟讀取效果
+  const loading = loadingHandler.show()
   setTimeout(() => {
     // 關閉讀取效果
+    loading.hide()
   }, 1000);
 }
 </script>
