7c5eca5d0532423ea83f598c9215fa633977868d
Ariel <sonyt605@gmail.com>
2024-12-11 23:55:28 +0800

    add booking store

--- stores/booking.js
+++ stores/booking.js
@@ -1,1 +1,11 @@
-// export const useBookingStore = 
+export const useBookingStore = defineStore("booking", () => {
+    const bookingInfo = ref({});
+    const setBookingInfo = (bookingData) => {
+        bookingInfo.value = bookingData;
+    };
+
+    return {
+        bookingInfo,
+        setBookingInfo
+    }
+})
